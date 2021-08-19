f36a6f45684e090ed6853773b407c43b61f7d71a
Lukasz <lukasz.uszko@aptiv.com>
2021-08-19 14:28:05 +0200

    tmux update

--- tmux/install.sh
+++ tmux/install.sh
@@ -2,12 +2,16 @@
 
 THIS_DIR=`dirname "$(readlink -f "$0")"`
 
+# Clear all config if exists
+rm -rf ~/.tmux
+rm -f ~/.tmux.conf
+
 # Install tmux
 sudo apt install tmux
 
 ln -sfT $THIS_DIR/.tmux.conf ~/.tmux.conf
 
 # install tmux plugins
-mkdir -p ~/.tmux/plugins && git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm && ~/.tmux/plugins/tpm/bin/install_plugins
+mkdir -p ~/.tmux/plugins && cd ~/.tmux/plugins && git clone https://github.com/tmux-plugins/tpm && ~/.tmux/plugins/tpm/bin/install_plugins
 
 echo "TMUX config done!"
