62aafa6bd97be70917a3595398b785bacc7bdd07
Lukasz <lukasz.uszko@aptiv.com>
2021-08-19 13:40:25 +0200

    Update

--- nvim/install.sh
+++ nvim/install.sh
@@ -1,16 +1,15 @@
 #!/bin/sh
 
 # Install neovim
-sudo apt install python3-dev
-sudo apt install cmake
+sudo apt install curl
 sudo apt install neovim
 
 THIS_DIR=`dirname "$(readlink -f "$0")"`
 echo $THIS_DIR
 ln -sf $THIS_DIR/ ~/.config/nvim
 
-curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 # Optional, if you still use vim
 ln -sf ~/.config/nvim/init.vim ~/.vimrc
