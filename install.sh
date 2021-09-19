d3d2f1f77c8df3ae1542fc7e97a447e4704f7341
luk6xff <lukasz.uszko@gmail.com>
2021-09-19 16:16:24 +0200

    Update tools list used

--- install.sh
+++ install.sh
@@ -1,6 +1,32 @@
 #!/bin/sh
 
 # The script is going to work only on UBUNTU based distros
+echo "Install and setup all the needed tools..."
+
+## git
+sudo apt install git
+git config --global user.email "lukasz.uszko@gmail.com"
+git config --global user.name "luk6xff"
+## gitk
+sudo apt install gitk
+## vscode
+sudo apt install code
+## minicom
+sudo apt install minicom
+sudo usermod -a -G tty $USER
+sudo usermod -a -G dialout $USER
+## mc
+sudo apt install mc
+## vlc
+sudo apt install vlc
+## sshpass
+sudo apt install sshpass
+## flameshot
+sudo snap install flameshot
+
+## Generate sshkeys
+ssh-keygen -t rsa -b 4096 -C "lukasz.uszko@gmail.com"
+#cat ~/.ssh/id_rsa.pub > https://github.com/settings/keys
 
 # We like the XDG_CONFIG standard, so make sure it is set up.
 export XDG_CACHE_HOME=${XDG_CACHE_HOME:=${HOME}/.cache}
