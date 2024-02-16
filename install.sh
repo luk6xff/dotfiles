6a9e9ac12ac7d87902f84094943747c1e407b4aa
luk6xff <lukasz.uszko@volvocars.com>
2024-02-16 09:36:30 +0100

    Add ssh keys perms

--- install.sh
+++ install.sh
@@ -27,11 +27,9 @@ sudo snap install flameshot
 ## Generate sshkeys
 ssh-keygen -t rsa -b 4096 -C "lukasz.uszko@gmail.com"
 #cat ~/.ssh/id_rsa.pub > https://github.com/settings/keys
+# Update permissions
+cd ~ && chmod 600 ~/.ssh/* && chmod 700 ~/.ssh && chmod 644 ~/.ssh/*.pub
 
-# We like the XDG_CONFIG standard, so make sure it is set up.
-export XDG_CACHE_HOME=${XDG_CACHE_HOME:=${HOME}/.cache}
-export XDG_CONFIG_HOME=${XDG_CONFIG_HOME:=${HOME}/.config}
-export XDG_DATA_HOME=${XDG_DATA_HOME:=${HOME}/.local/share}
 
 # Find the location of this file and the dotfile repo directory.
 DOTFILES=`dirname "$(readlink -f "$0")"`
