41c421c9bc1e7cfced346af303aa35fec9a11c91
luk6xff <lukasz.uszko@volvocars.com>
2024-02-23 01:18:51 +0100

    vscode update

--- vscode/install.sh
+++ vscode/install.sh
@@ -3,8 +3,23 @@
 SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 
 # vscode
+sudo apt-get install -y wget gpg
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+rm -f packages.microsoft.gpg
+sudo apt install -y apt-transport-https
+sudo apt update
 sudo apt install -y code
-# or 
+# or
 #sudo snap install code --classic
 
+# settings
 ln -sfT ${SCRIPT_DIR}/settings.json ~/.config/Code/User/settings.json
+
+# extensions
+code --install-extension ms-python.python
+code --install-extension ms-vscode.cpptools
+code --install-extension eamodio.gitlens
+code --install-extension github.copilot
+code --install-extension rust-lang.rust-analyzer
